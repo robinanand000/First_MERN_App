8ef006c1e26995254ac1e5274834fbf718afc589
Robins Anand <robinsanand222@gmail.com>
2025-10-01 03:31:57 +0530

    updated app name in navbar

--- Frontend/src/Shared/Components/Navigation/MainNavigation.js
+++ Frontend/src/Shared/Components/Navigation/MainNavigation.js
@@ -33,7 +33,7 @@ const MainNavigation = (props) => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">Your Places</Link>
+          <Link to="/">PlaceBook</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
